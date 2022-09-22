86a2c8193b754aa1a46feb38382a7c685ef50abb
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-22 00:44:32 -0300

    feat: create footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import FormLogin from "./components/FormLogin";
 import Header from "./components/Header";
 import FormSignUp from "./components/FormSignUp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      {<Header />}
-      <FormSignUp />
+      {/* {<Header />}
+      <FormSignUp /> */}
       {/* <FormLogin/> */}
+      {<Footer />}
     </>
   );
 }
