0127383e89f19313efc51a6f98b1ff0c2798bb53
froyalt <fernandotorreal@hotmail.com>
2022-09-22 00:01:40 -0300

    feat: creating Header, renaming files

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { StyledNav } from './styles';
+import "./styles.scss"
+
+
+export default function Header() {
+  return (
+    <StyledNav>
+    <Navbar id="view-height" className='bg-info'>
+      <Container>
+        <Navbar.Brand className='text-white' href="#home">📷 Logomarca</Navbar.Brand>
+        <Navbar.Brand className='text-white' href="#home">Home</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+            <Button className='py-1 px-4 rounded-1'>Login</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </StyledNav>
+  );
+}
