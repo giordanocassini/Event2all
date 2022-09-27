22625b20ca40e8853932ff39b06944996a5d780b
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-09-27 01:27:29 -0300

    feat : formSignUp styles

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -2,7 +2,7 @@ import { Form, Card, FormGroup, Button } from "react-bootstrap";
 import { FormEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { register } from "../../services/auth";
-
+import "./formsign.scss";
 /* import { cadastro } from "../../services/userServices";
 import { useNavigate } from "react-router-dom"; */
 export default function FormSignUp() {
@@ -46,61 +46,60 @@ export default function FormSignUp() {
         className="formCadastro container h-100 pt-5 d-flex align-itens-center 
         justify-content-center  "
       >
-        <Card className="cardForm ">
-          <Form className="formulario text-center">
-            <img src="/assets/img/logo-colorido.png" alt="" />
-            <h3> Crie sua conta agora!</h3>
-            <span>Chegou a hora de planejar eventos incríveis!</span>
-            <Form.Group
-              className=" boxform d-flex  p-3 "
-              controlId="formBasicEmail"
-            >
-              <Form.Control
-                className="inputTexto"
-                type="text"
-                placeholder="Digite seu nome"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="boxform p-3" controlId="formBasicEmail">
-              <Form.Control
-                className="inputTexto"
-                type="email"
-                placeholder="Digite seu melhor e-mail"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+        <Form className="formulario text-center">
+          <img src="/assets/img/logo-colorido.png" alt="" />
+          <h3> Crie sua conta!</h3>
+          <span>Preencha seus dados e programe seu evento já!</span>
+          <Form.Group
+            className=" boxform d-flex  p-3 "
+            controlId="formBasicEmail"
+          >
+            <Form.Control
+              className="inputTexto"
+              type="text"
+              placeholder="Digite seu nome"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="boxform p-3" controlId="formBasicEmail">
+            <Form.Control
+              className="inputTexto"
+              type="email"
+              placeholder="Digite seu  e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-            <Form.Group className=" boxform p-3" controlId="formBasicPassword">
-              <Form.Control
-                className="inputTexto"
-                type="password"
-                placeholder="Digite sua senha"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="p-3 boxform" controlId="formBasicPassword">
-              <Form.Control
-                className={`inputTexto ${wrongPass}`}
-                type="password"
-                placeholder="Confirme sua senha"
-                value={confirmarPassword}
-                onChange={(e) => setConfirmarPassword(e.target.value)}
-              />
-            </Form.Group>
+          <Form.Group className=" boxform p-3" controlId="formBasicPassword">
+            <Form.Control
+              className="inputTexto"
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="p-3 boxform" controlId="formBasicPassword">
+            <Form.Control
+              className={`inputTexto ${wrongPass}`}
+              type="password"
+              placeholder="Confirme sua senha"
+              value={confirmarPassword}
+              onChange={(e) => setConfirmarPassword(e.target.value)}
+            />
+          </Form.Group>
 
-            <Button className="button" type="submit">
-              Fazer cadastro
-            </Button>
-            <FormGroup className="cadastrarLink">
-              <p>Já tem cadastro?</p>
-              <Link to={"/login"}>Login</Link>
-            </FormGroup>
-          </Form>
-        </Card>
+          <Button className="buttonCadastro" type="submit">
+            Fazer cadastro
+          </Button>
+          <FormGroup className="cadastrarLink">
+            <p className="p-4">
+              Já tem cadastro? <Link to={"/login"}>Login</Link>
+            </p>
+          </FormGroup>
+        </Form>
       </div>
     </div>
   );
