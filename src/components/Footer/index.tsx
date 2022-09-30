923c50d3e1c176a36bc4f360b62993ff2919e0a5
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-09-30 17:43:25 -0300

    feat: link footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,37 @@
 import Navbar from "react-bootstrap/Navbar";
 import "./footer.scss";
-
+import { Link } from "react-router-dom";
 export default function Footer(props: any) {
   return (
-    <Navbar id="asd" style={{backgroundColor: props.backgroundColor, position: props.position}}>
+    <Navbar
+      id="asd"
+      style={{
+        backgroundColor: props.backgroundColor,
+        position: props.position,
+      }}
+    >
       <div className="footer">
-        <Navbar.Brand  className="logoFooter" href="#home">
-          <img src={`../../../public/images/${props.logotipo}`}></img>
+        <Navbar.Brand className="logoFooter" href="#home">
+          <Link to={"/"}>
+            <img src={`../../../public/images/${props.logotipo}`}></img>
+          </Link>
         </Navbar.Brand>
         <Navbar.Brand href="#facebook">
           <img src="../../../public/images/face.png"></img>
         </Navbar.Brand>
         <Navbar.Brand href="#instagram">
-        <img src="../../../public/images/insta.png"></img>
+          <img src="../../../public/images/insta.png"></img>
         </Navbar.Brand>
         <Navbar.Brand href="#google">
-        <img src="../../../public/images/img.png"></img>
+          <img src="../../../public/images/img.png"></img>
         </Navbar.Brand>
         <Navbar.Brand href="#ios">
-        <img src="../../../public/images/ios.png"></img>
+          <img src="../../../public/images/ios.png" />
         </Navbar.Brand>
       </div>
-      <div className="copyright" style={{color: props.color}}>© Todos os direitos reservados. Event2All.</div>
+      <div className="copyright" style={{ color: props.color }}>
+        © Todos os direitos reservados. Event2All.
+      </div>
     </Navbar>
   );
 }
