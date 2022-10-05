985fb657811fd99d1104859f11a5137c7cc76642
froyalt <fernandotorreal@hotmail.com>
2022-10-04 23:19:06 -0300

    feat: adding persistor

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReduce from "./modules/users";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit"
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage
+};
+
+const reducer = combineReducers({
+  userReduce,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 const store = configureStore({
-  reducer: { userReduce },
+  reducer: persistedReducer
 });
 
 export type RootStore = ReturnType<typeof store.getState>;
