0fafae023a3bdcb8c18d2a5e06aac307a1678ef8
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-10-05 21:52:07 -0300

    feat: table budget page

--- src/components/BudgetPage/index.tsx
+++ src/components/BudgetPage/index.tsx
@@ -1,37 +1,99 @@
 import SideBar from "../SideBar/SideBar";
-import { Card, Button } from "react-bootstrap";
-import "./BudgetPage.scss"
+import { Card, Button, Table } from "react-bootstrap";
+import "./BudgetPage.scss";
 
 export default function BudgetPage() {
   return (
     <>
       <div className="h-100 d-flex">
         <SideBar />
-        <div className="vh-100 d-flex justify-content-center flex-row w-100">
-          <div>
-            <Card id="card-budget" className="rounded text-center m-4">
-              <Card.Body className="mt-2">
-                <Card.Title className="text-black">Previsto: asdasdasd</Card.Title>
-              </Card.Body>
-            </Card>
+        <div className="d-flex flex-column">
+          <div className="m-5 d-flex justify-content-center flex-row w-100">
+            <div>
+              <Card id="card-budget" className="rounded text-center m-4">
+                <Card.Body className="mt-2">
+                  <Card.Title className="text-black">Previsto:</Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+            <div>
+              <Card id="card-budget2" className="rounded text-center m-4">
+                <Card.Body className="mt-2">
+                  <Card.Title className="text-black">Atual:</Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+            <div>
+              <Card id="card-budget3" className="rounded text-center m-4">
+                <Card.Body className="mt-2">
+                  <Card.Title className="text-black">Restante:</Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
-          <div>
-            <Card id="card-budget2" className="rounded text-center m-4">
-              <Card.Body className="mt-2">
-              <Card.Title className="text-black">Atual:</Card.Title>
-              </Card.Body>
-            </Card>
+          <div className="d-flex justify-content-end">
+            <Button className="primary m-4">+ Adicionar Despesa</Button>
           </div>
-          <div>
-            <Card id="card-budget3" className="rounded text-center m-4">
-              <Card.Body className="mt-2">
-              <Card.Title className="text-black">Restante:</Card.Title>
-              </Card.Body>
-            </Card>
+
+          <div className="vh-100 d-flex justify-content-center flex-row w-100 m-4">
+            <Table className="m-4 ">
+              <thead>
+                <tr>
+                  <th>DESCRIÇÃO</th>
+                  <th>FORNECEDOR</th>
+                  <th>CONTATO</th>
+                  <th>PREVISTO</th>
+                  <th>CONTRATO</th>
+                  <th>PAGO</th>
+                  <th>DIFERENÇA</th>
+                  <th>STATUS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
         </div>
-
-
       </div>
     </>
   );
