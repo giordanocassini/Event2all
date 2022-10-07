2f077792012e9dcd0663486e74fd7ad37ef5904d
froyalt <fernandotorreal@hotmail.com>
2022-10-07 00:14:25 -0300

    refactor: EventButton

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -45,12 +45,6 @@ export default function SideBar() {
         </span>
         <hr className="mb-4" />
         <EventList />
-        {/* <div id="test-asd" className="d-flex flex-column p-4 mt-3">
-              <h5>Você não tem nenhum evento adicionado.</h5>
-              <p className="text-white">Para adicionar, clique no botão abaixo ou no '+'</p>
-              <hr />
-              <CreateEvent />
-            </div> */}
       </div>
     </div>
   );
