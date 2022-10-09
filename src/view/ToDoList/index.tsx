e5d64127345818be3680f930cdc72ed68a131415
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-10-09 13:37:34 -0300

    feat: add BreadCrumbs in the ToDoList

--- src/view/ToDoList/index.tsx
+++ src/view/ToDoList/index.tsx
@@ -2,15 +2,21 @@ import SideBar from "../../components/SideBar/SideBar";
 import BreadCrumbs from "../../components/BreadCrumbs";
 
 export default function ToDoList() {
+  const breadCrumbsItem = [
+    { name: "Home", link: "/" },
+    { name: "Nome do Evento", link: "/evento" },
+    { name: "Convidados", link: "/convidados" },
+  ];
+
   return (
     <>
       <div className="h-100 d-flex">
         <SideBar />
         <div className="w-100 vh-100">
           <div>
-            {/* <div>
+            <div>
               <BreadCrumbs items={breadCrumbsItem} />
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
