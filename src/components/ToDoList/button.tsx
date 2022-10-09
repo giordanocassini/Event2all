04800ffc801dbfd02c369113ed1dbf35c66e8d70
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-10-09 20:20:01 -0300

    fix : geral button

--- src/components/ToDoList/button.tsx
+++ src/components/ToDoList/button.tsx
@@ -1,15 +1,17 @@
-import { useContext } from 'react';
-import { TasksContext } from '../../contexts/TasksContext';
-import './button.scss';
+import { useContext } from "react";
+import { TasksContext } from "../../contexts/TasksContext";
+import "./button.scss";
 
 export function Button() {
-    const { handleAddTasks } = useContext(TasksContext);
-    return (
-        <button
-            className="btn"
-            onClick={() => {handleAddTasks()}}
-        >
-            Salvar
-        </button>
-    );
+  const { handleAddTasks } = useContext(TasksContext);
+  return (
+    <button
+      className="botaoToDoList"
+      onClick={() => {
+        handleAddTasks();
+      }}
+    >
+      Salvar
+    </button>
+  );
 }
