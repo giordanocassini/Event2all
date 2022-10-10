021487910eb9e0a4d4ee5ecc2716244be2014f19
froyalt <fernandotorreal@hotmail.com>
2022-10-10 16:02:47 -0300

    refactor: cadastroEvent

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
-export default axios.create({
+const baseAPI = axios.create({
   baseURL: "https://apievent2all.herokuapp.com",
+  // headers: {
+  //   "content-type" :"application/json",
+  //   "accept": "*/*",
+  //   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA0LCJpYXQiOjE2NjU0MjgxNjQsImV4cCI6MTY2NTQ1Njk2NH0.JXpbJ9s86z-oO5SujPzYwSNrgtKqZCgqn8Eb4HNaTzA"
+
+  // }
 });
+
+export default baseAPI;
