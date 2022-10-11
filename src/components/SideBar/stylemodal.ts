338b148f9836d22fbfe7fb6514c83f6a9d688107
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-10-11 11:13:04 -0300

    revert : event/id

--- src/components/SideBar/stylemodal.ts
+++ src/components/SideBar/stylemodal.ts
@@ -4,7 +4,7 @@ export const ButtonCreateModal = styled.button`
   background-color: #14c989;
   border-bottom: 2px solid #0a6545;
   border-radius: 4.8px;
-  width: 15.2rem;
+  width: 15.4rem;
   font-size: 21px;
   font-weight: 700;
   color: #191933;
