351ca09231544a4ce791a4adf7e01e16845463af
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-10-12 13:17:00 -0300

    refactor : api settings

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,11 +1,18 @@
 import baseApi from "./api";
 
 const token = window.localStorage.getItem("token");
+const config = {
+  headers: {
+    Authorization: "Bearer " + token,
+  },
+};
 
-export function pegaUsuarioPorId(id: string | null, token: string | null) {
-  return baseApi.get(`user/${id}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+export function pegaUsuarioPorId(id: string | number | null) {
+  return baseApi.get(`user/${id}`, config);
+}
+
+export function getEventListByUser(id: number) {
+  return baseApi.get(`/event/${id}`, config);
 }
 
 // export function editarUsuario(id: number, payload: EditarUsuario) {
