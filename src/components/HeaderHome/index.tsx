4024e3756394636947f020126b5db35f0f146fba
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-10-12 20:32:33 -0300

    refactor: responsive home mobile

--- src/components/HeaderHome/index.tsx
+++ src/components/HeaderHome/index.tsx
@@ -9,19 +9,18 @@ export default function HeaderHome() {
   return (
     <Navbar className="d-flex dark-blue p-4">
       <Container id="mediaImg">
-        <Navbar.Brand className="text-white" href="#home">
+        <Navbar.Brand href="#home">
           <img className="logo-header" src="/images/logotipo2.png"></img>
         </Navbar.Brand>
-
         <Navbar.Toggle />
         <Navbar.Collapse id="media" className="justify-content-end">
           <Link className="button-header" to={"/login"}>
-            <Button className="btn-light rounded-1 text-dark fw-bold button-login">
+            <Button className="mx-1 rounded-1 fw-bold button-login">
               Fazer login
             </Button>
           </Link>
           <Link className="button-header" to={"/cadastro"}>
-            <Button className="btn-success rounded-1 fw-bold button-signup">
+            <Button className="mx-1 rounded-1 fw-bold button-signup">
               Cadastrar-se
             </Button>
           </Link>
