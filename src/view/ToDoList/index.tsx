7c9408b7fc6cfd3a9e0589c99bd120d1a2fb8776
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-10-13 12:03:50 -0300

    styles : todo update

--- src/view/ToDoList/index.tsx
+++ src/view/ToDoList/index.tsx
@@ -7,7 +7,6 @@ import { AiFillCheckCircle } from "react-icons/ai";
 import { Form, InputGroup } from "react-bootstrap";
 import "./toDo.scss";
 
-
 export default function ToDoList() {
   const breadCrumbsItem = [
     { name: "Dashboard", link: "/dashboard" },
@@ -16,47 +15,55 @@ export default function ToDoList() {
   ];
 
   return (
-    <div className="h-100 d-flex">
-      <SideBar />
-      <div className="w-100 vh-100 d-flex flex-column">
-          <BreadCrumbs className="" items={breadCrumbsItem} />
-        <div className="headerTarefas w-100">
-          <div className="listaTarefas">
-            <AiFillCheckCircle className="me-2" />
-            Lista de Tarefas
-          </div>
-          <div className="novasTarefas">
-            <NewTask />
-          </div>
-        </div>
-        <div>
-          <input></input>
-        </div>
-        <div>tasks</div>
-      </div>
-    </div>
-  );
-}
+    // <div className="h-100 d-flex">
+    //   <SideBar />
+    //   <div className="w-100 vh-100 d-flex flex-column">
+    //
+    //     <div className="headerTarefas w-100">
+    //       <div className="listaTarefas">
+    //         <AiFillCheckCircle className="me-2" />
+    //         Lista de Tarefas
+    //       </div>
+    //       <div className="novasTarefas">
+    //         <NewTask />
+    //       </div>
+    //     </div>
+    //     <div>
+    //       <input></input>
+    //     </div>
+    //     <div>tasks</div>
+    //   </div>
+    // </div>
 
-
-{/* <>
+    <>
       <div className="h-100 d-flex">
         <SideBar />
-        <div className="w-100 vh-100 "></div>
-            <div className="">
-            <div className="d-flex align-items-center justify-content-center">
-              <AiFillCheckCircle className="me-2" />
-              Lista de Tarefas
+        <div className="d-flex flex-column w-100">
+          <div>
+            <BreadCrumbs items={breadCrumbsItem} />
+          </div>
+
+          <div className=" d-flex w-100 ">
+            <div className="d-flex ">
+              <AiFillCheckCircle className="me-2"></AiFillCheckCircle>
+              <p>Lista de Tarefas</p>
+              <div>
+                <TasksProvider className="d-flex ">
+                  <NewTask />
+                  <InputGroup className="mt-2 px-1 mb-1">
+                    <Form.Control
+                      placeholder="Buscar..."
+                      aria-label="Buscar"
+                      aria-describedby="basic-addon2"
+                    />
+                  </InputGroup>
+                  <TaskList />
+                </TasksProvider>
+              </div>
             </div>
-            {/* <InputGroup className="mt-2 px-1 mb-1">
-              <Form.Control
-                placeholder="Buscar..."
-                aria-label="Buscar"
-                aria-describedby="basic-addon2"
-              />
-            </InputGroup> */}
-           /*  <TasksProvider>
-              <NewTask />
-              <TaskList />
-            </TasksProvider>
-            </> */
+          </div>
+        </div>
+      </div>{" "}
+    </>
+  );
+}
