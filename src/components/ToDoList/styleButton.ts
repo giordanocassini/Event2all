400ce32f020ef58541c1ace6264193ba588a94c1
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-10-13 00:20:56 -0300

    refactor: toDoList and hover button formLogin

--- src/components/ToDoList/styleButton.ts
+++ src/components/ToDoList/styleButton.ts
@@ -3,21 +3,9 @@ import styled from "styled-components";
 export const ButtonToDo = styled.button`
   width: 100%;
   max-width: 170px;
-  padding: 10px 20px;
-  border: 2px solid #14c989;
-  background-color: #14c989;
   border-radius: 5px;
   font-weight: 700;
   color: #e6e6ee;
   cursor: pointer;
-  font-size: 1.12rem;
-  font-weight: 500;
-  transition: filter 0.2s;
-`;
 
-// @media (max-width: 576px) {
-//   button.btn {
-//     width: 100%;
-//     max-width: 100%;
-//   }
-//
+`;
