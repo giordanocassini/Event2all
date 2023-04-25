550151685b846fb291d02c10e09e803c607a5b8a
giordano.cassini <giordano.cassini@gmail.com>
2023-04-25 16:38:48 -0300

    fix: geting vite config right for docker

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-  baseURL: "localhost:3000",
+  baseURL: "http://localhost:3000/",
   // headers: {
   //   "content-type" :"application/json",
   //   "accept": "*/*",
